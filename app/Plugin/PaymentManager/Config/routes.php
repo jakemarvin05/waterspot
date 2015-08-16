4e1ec218985915b9b94940ea1f99944bb8c2ea78
po-cruz <po.cruz17@gmail.com>
2015-08-16 23:57:38 +0800

    changed it back

--- app/Plugin/PaymentManager/Config/routes.php
+++ app/Plugin/PaymentManager/Config/routes.php
@@ -3,7 +3,6 @@
  Router::connect('/payment/payment_summary/*', array('plugin'=>'payment_manager','controller' => 'payments', 'action' => 'payment_summary'));
  Router::connect('/payment/cancelled_url/*', array('plugin'=>'payment_manager','controller' => 'payments', 'action' => 'cancelled_url'));
  Router::connect('/payment/failled_url/*', array('plugin'=>'payment_manager','controller' => 'payments', 'action' => 'failled_url'));
- Router::connect('/payment/failled_url/*', array('plugin'=>'payment_manager','controller' => 'payments', 'action' => 'failled_url'));
 
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
